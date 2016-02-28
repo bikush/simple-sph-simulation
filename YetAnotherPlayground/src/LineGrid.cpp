6b697733d33208c23a9363ea366c35916b1191fc
Bruno Mikus <bikush.mruno@gmail.com>
2016-02-28 17:33:04 +0100

    Coordinate system vizualization in LineGrid.

--- YetAnotherPlayground/src/LineGrid.cpp
+++ YetAnotherPlayground/src/LineGrid.cpp
@@ -48,6 +48,61 @@ LineGrid::LineGrid(int xCount, float deltaX, float offsetX, int yCount, float de
 	delete[] elements;
 }
 
+LineGrid::LineGrid( float length, float lineWidth ) : Object3D(), lineWidth(lineWidth)
+{
+	int elementCount = 18;
+	int vertexCount = 12 * 6;
+	GLfloat* vertices = new GLfloat[vertexCount];
+	GLuint* elements = new GLuint[elementCount];
+
+	float arrowEnd = length * 0.9f;
+	float arrowOffset = length * 0.07f;
+
+	// x-Axis
+	fillData(&vertices[0], { -length,0.0f,0.0f }, { 1.0,0.0f,0.0f });	
+	fillData(&vertices[1*6], { length,0.0,0.0f }, { 1.0f,0.0f,0.0f });	
+	fillData(&vertices[2*6], { arrowEnd,0.0f,arrowOffset }, { 1.0,0.0f,0.0f });
+	fillData(&vertices[3*6], { arrowEnd,0.0f,-arrowOffset }, { 1.0f,0.0f,0.0f });
+
+	elements[0] = 0;
+	elements[1] = 1;
+	elements[2] = 1;
+	elements[3] = 2;
+	elements[4] = 1;
+	elements[5] = 3;
+	
+	// y-Axis
+	fillData(&vertices[4*6], { 0.0f,-length,0.0f }, { 0.0,1.0f,0.0f });
+	fillData(&vertices[5*6], { 0.0f,length,0.0f }, { 0.0f,1.0f,0.0f });
+	fillData(&vertices[6*6], { arrowOffset,arrowEnd,0.0f }, { 0.0,1.0f,0.0f });
+	fillData(&vertices[7*6], { -arrowOffset,arrowEnd,0.0f }, { 0.0f,1.0f,0.0f });
+
+	elements[6] = 4;
+	elements[7] = 5;
+	elements[8] = 5;
+	elements[9] = 6;
+	elements[10] = 5;
+	elements[11] = 7;
+
+	// z-Axis
+	fillData(&vertices[8*6], { 0.0f,0.0f,-length }, { 0.0,0.0f,1.0f });
+	fillData(&vertices[9*6], { 0.0f,0.0f,length }, { 0.0f,0.0f,1.0f });
+	fillData(&vertices[10*6], { 0.0f,arrowOffset,arrowEnd }, { 0.0,0.0f,1.0f });
+	fillData(&vertices[11*6], { 0.0f,-arrowOffset,arrowEnd }, { 0.0f,0.0f,1.0f });
+
+	elements[12] = 8;
+	elements[13] = 9;
+	elements[14] = 9;
+	elements[15] = 10;
+	elements[16] = 9;
+	elements[17] = 11;
+
+	initializeVBO(vertices, vertexCount * sizeof(GLfloat), elements, 18 * sizeof(GLuint), GL_LINES);
+
+	delete[] vertices;
+	delete[] elements;
+}
+
 LineGrid::~LineGrid()
 {
 }
--- YetAnotherPlayground/src/LineGrid.h
+++ YetAnotherPlayground/src/LineGrid.h
@@ -8,6 +8,7 @@ private:
 
 public:
 	LineGrid( int xCount, float deltaX, float offsetX, int yCount, float deltaY, float offsetY);
+	LineGrid(float length, float lineWidth = 1.0f);
 	~LineGrid();
 
 	void draw(const glm::mat4& viewProjection);
